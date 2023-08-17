c9e29348060f0d13699022fa58c61b233d48f5f2
u6k <u6k.apps@gmail.com>
2023-08-17 22:28:52 +0900

    Added: カレンダーページのパース処理をテストする

--- boatrace_crawler/settings.py
+++ boatrace_crawler/settings.py
@@ -1,93 +1,52 @@
-# Scrapy settings for boatrace_crawler project
-#
-# For simplicity, this file contains only settings considered important or
-# commonly used. You can find more settings consulting the documentation:
-#
-#     https://docs.scrapy.org/en/latest/topics/settings.html
-#     https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
-#     https://docs.scrapy.org/en/latest/topics/spider-middleware.html
+import logging
+import os
+from distutils.util import strtobool
 
 BOT_NAME = "boatrace_crawler"
+USER_AGENT = os.environ.get("USER_AGENT", "boatrace_crawler/1.0 (+https://github.com/u6k/boatrace-crawler)")
+# TODO: CRAWL_HTTP_PROXY = os.environ.get("CRAWL_HTTP_PROXY")
 
 SPIDER_MODULES = ["boatrace_crawler.spiders"]
 NEWSPIDER_MODULE = "boatrace_crawler.spiders"
 
-
-# Crawl responsibly by identifying yourself (and your website) on the user-agent
-# USER_AGENT = "boatrace_crawler (+http://www.yourdomain.com)"
-
-# Obey robots.txt rules
 ROBOTSTXT_OBEY = True
 
-# Configure maximum concurrent requests performed by Scrapy (default: 16)
-# CONCURRENT_REQUESTS = 32
+CONCURRENT_REQUESTS = 1
+CONCURRENT_REQUESTS_PER_DOMAIN = 1
+CONCURRENT_REQUESTS_PER_IP = 0
 
-# Configure a delay for requests for the same website (default: 0)
-# See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
-# See also autothrottle settings and docs
-# DOWNLOAD_DELAY = 3
-# The download delay setting will honor only one of:
-# CONCURRENT_REQUESTS_PER_DOMAIN = 16
-# CONCURRENT_REQUESTS_PER_IP = 16
+DOWNLOAD_DELAY = 1
+DOWNLOAD_TIMEOUT = 60
+RETRY_TIMES = 10
 
-# Disable cookies (enabled by default)
-# COOKIES_ENABLED = False
+HTTPCACHE_ENABLED = True
+# TODO: HTTPCACHE_STORAGE = "horse_racing_crawler.middlewares.S3CacheStorage"
 
-# Disable Telnet Console (enabled by default)
-# TELNETCONSOLE_ENABLED = False
-
-# Override the default request headers:
-# DEFAULT_REQUEST_HEADERS = {
-#    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-#    "Accept-Language": "en",
-# }
-
-# Enable or disable spider middlewares
-# See https://docs.scrapy.org/en/latest/topics/spider-middleware.html
-# SPIDER_MIDDLEWARES = {
-#    "boatrace_crawler.middlewares.BoatraceCrawlerSpiderMiddleware": 543,
-# }
-
-# Enable or disable downloader middlewares
-# See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
-# DOWNLOADER_MIDDLEWARES = {
-#    "boatrace_crawler.middlewares.BoatraceCrawlerDownloaderMiddleware": 543,
-# }
-
-# Enable or disable extensions
-# See https://docs.scrapy.org/en/latest/topics/extensions.html
-# EXTENSIONS = {
-#    "scrapy.extensions.telnet.TelnetConsole": None,
-# }
-
-# Configure item pipelines
-# See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-# ITEM_PIPELINES = {
-#    "boatrace_crawler.pipelines.BoatraceCrawlerPipeline": 300,
-# }
-
-# Enable and configure the AutoThrottle extension (disabled by default)
-# See https://docs.scrapy.org/en/latest/topics/autothrottle.html
-# AUTOTHROTTLE_ENABLED = True
-# The initial download delay
-# AUTOTHROTTLE_START_DELAY = 5
-# The maximum download delay to be set in case of high latencies
-# AUTOTHROTTLE_MAX_DELAY = 60
-# The average number of requests Scrapy should be sending in parallel to
-# each remote server
-# AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
-# Enable showing throttling stats for every response received:
-# AUTOTHROTTLE_DEBUG = False
-
-# Enable and configure HTTP caching (disabled by default)
-# See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
-# HTTPCACHE_ENABLED = True
-# HTTPCACHE_EXPIRATION_SECS = 0
-# HTTPCACHE_DIR = "httpcache"
-# HTTPCACHE_IGNORE_HTTP_CODES = []
-# HTTPCACHE_STORAGE = "scrapy.extensions.httpcache.FilesystemCacheStorage"
+SPIDER_CONTRACTS = {
+    "boatrace_crawler.contracts.CalendarContract": 10,
+}
 
 # Set settings whose default value is deprecated to a future-proof value
 REQUEST_FINGERPRINTER_IMPLEMENTATION = "2.7"
 TWISTED_REACTOR = "twisted.internet.asyncioreactor.AsyncioSelectorReactor"
 FEED_EXPORT_ENCODING = "utf-8"
+
+logging.getLogger("boto3").setLevel(logging.INFO)
+logging.getLogger("botocore").setLevel(logging.INFO)
+
+AWS_ENDPOINT_URL = os.environ["AWS_ENDPOINT_URL"]
+AWS_ACCESS_KEY_ID = os.environ["AWS_ACCESS_KEY_ID"]
+AWS_SECRET_ACCESS_KEY = os.environ["AWS_SECRET_ACCESS_KEY"]
+AWS_S3_CACHE_BUCKET = os.environ["AWS_S3_CACHE_BUCKET"]
+AWS_S3_CACHE_FOLDER = os.environ["AWS_S3_CACHE_FOLDER"]
+
+RECACHE_RACE = strtobool(os.environ.get("RECACHE_RACE", "False"))
+RECACHE_DATA = strtobool(os.environ.get("RECACHE_DATA", "False"))
+
+# TODO
+# FEEDS = {
+#     os.environ["AWS_S3_FEED_URL"]: {
+#         "format": "json",
+#         "encoding": "utf8",
+#     }
+# }
